ab4645b8fcb67adca2010c92119c7f0274501b3d
Emmanuel <emmy@MacBookAir.lan>
2026-06-18 01:31:22 +0100

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,71 @@
+"use client"
+
+import HeroSection from "@/components/HeroSection";
+import { API_URL, IMAGE_PATH } from "@/constants";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [movies, setMovies] = useState<IMovie[]>([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const fetchMovies = async (query = "") => {
+    try {
+      const response = await fetch(
+        query
+        ? `${API_URL}/search/movie?query=${encodeURIComponent(query)}` :`${API_URL}/discover/movie`, {
+        headers: {
+          accept: "application/json",
+          Authorization: `Bearer ${process.env.NEXT_PUBLIC_TMDB_API_KEY}`,
+        },
+      });
+      const data = await response.json();
+      setMovies(data.results || []);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+    useEffect(() => {
+      fetchMovies();
+    }, []);
+
+    useEffect(() => {
+      const timer = setTimeout(() => {
+        fetchMovies(searchTerm);
+      }, 500);
+
+      return () => {
+        clearTimeout(timer);
+      };
+    }, [searchTerm]);
+
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
+    <div>
+      <HeroSection movies={movies.slice(0, 5)} searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      <div className="flex flex-col m-10 mt-10">
+        <div className="mb-10">
+          <h2 className="text-3xl font-bold text-alabaster mb-3"> Popular Movies</h2>
+          <p className="text-lg text-santas-gray">Explore the most popular movies of the moment
           </p>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+      </div>
+
+      <div className="grid grid-cols-5 gap-6">
+        {movies.map((movie) => (
+        <div key={movie.id} className="group">
+          <div className="relative overflow-hidden cursor-pointer group rounded-xl">
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
+            className="group-hover:scale-110 duration-500 h-full w-full object-cover"
+            src={movie.poster_path ? `${IMAGE_PATH}${movie.poster_path}` : "/placeholder-image.svg"}
+              alt={movie.title}
+              width={250}
+              height={250}
             />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+          </div>
         </div>
-      </main>
+        ))}
+      </div>
     </div>
   );
-}
+};
---
+++ types/movie.d.ts
@@ -0,0 +1,8 @@
+interface IMovie {
+    title: string;
+    id: number;
+    vote_average: number;
+    overview: string;
+    release_date: string;
+    poster_path: string
+}
